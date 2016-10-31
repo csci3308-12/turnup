a2f585f4f6de450a55817bbd0ac97ec49307540d
Connor Hudson <technoboy10@users.noreply.github.com>
2016-10-31 14:29:57 -0700

    Add files via upload

---
+++ milestone3.sql
@@ -0,0 +1,108 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for osx10.9 (x86_64)
+--
+-- Host: us-cdbr-azure-central-a.cloudapp.net    Database: turnup
+-- ------------------------------------------------------
+-- Server version	5.5.45-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `song_info`
+--
+
+DROP TABLE IF EXISTS `song_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `song_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) DEFAULT NULL,
+  `artist` varchar(45) DEFAULT NULL,
+  `album` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `song_info`
+--
+
+LOCK TABLES `song_info` WRITE;
+/*!40000 ALTER TABLE `song_info` DISABLE KEYS */;
+INSERT INTO `song_info` VALUES (1,'A Change Is Gonna Come','Sam Cooke','A Change Is Gonna Come'),(2,'Me and Mrs. Jones','Billy Paul','360 Degrees of Billy Paul'),(3,'Let\'s Stay Together','Al Green','Let\'s Stay Together'),(4,'What\'s Going On?','Marvin Gaye','What\'s Going On?'),(5,'My Girl','The Temptations','Emperors of Soul');
+/*!40000 ALTER TABLE `song_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `song_train`
+--
+
+DROP TABLE IF EXISTS `song_train`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `song_train` (
+  `id` int(11) NOT NULL,
+  `bpm` varchar(45) DEFAULT NULL,
+  `genre` varchar(45) DEFAULT NULL,
+  `likes` int(11) DEFAULT NULL,
+  `plays` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `song_train`
+--
+
+LOCK TABLES `song_train` WRITE;
+/*!40000 ALTER TABLE `song_train` DISABLE KEYS */;
+INSERT INTO `song_train` VALUES (1,'172','Soul',0,0),(2,'100','Soul',0,0),(3,'102','Soul',0,0),(4,'202','Soul',0,0),(5,'105','Soul',0,0);
+/*!40000 ALTER TABLE `song_train` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_data`
+--
+
+DROP TABLE IF EXISTS `user_data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_data` (
+  `id` int(11) NOT NULL,
+  `likes` int(11) DEFAULT NULL,
+  `dislikes` int(11) DEFAULT NULL,
+  `favorite_song` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_data`
+--
+
+LOCK TABLES `user_data` WRITE;
+/*!40000 ALTER TABLE `user_data` DISABLE KEYS */;
+INSERT INTO `user_data` VALUES (1,5,0,4),(2,3,2,5),(3,1,4,2),(4,2,3,3),(5,0,5,5);
+/*!40000 ALTER TABLE `user_data` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-10-31 15:28:18
