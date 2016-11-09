ae16e5cfac2148b07f97bc6e19de26902ba0643c
Daniel <dtrahan41@googlemail.com>
2016-11-09 03:35:59 -0700

    Added buttons and music playback. Cleaned up Ui.

---
+++ scripts/audioplayer.js
@@ -0,0 +1,47 @@
+
+var playlist = ["bensound-anewbeginning.mp3", "bensound-goinghigher.mp3", "bensound-happyrock.mp3"];
+var index = 0;
+
+/*-- Need Comments--*/
+/*--Songs from bensounds.com--*/
+
+function playSong(){
+  document.getElementById('aSong').play();
+}
+
+function pauseSong(){
+  document.getElementById('aSong').pause();
+}
+
+function stopSong(){
+  document.getElementById('aSong').load();
+  document.getElementById('aSong').pause();
+}
+
+function nextSong(){
+  document.getElementById('aSong').pause();
+  document.getElementById('aSong').removeAttribute("src");
+  document.getElementById('aSong').load();
+
+  index++;
+
+  if(index >= playlist.length ){ index = 0; }
+
+  document.getElementById('aSong').src = "music/" + playlist[index];
+  document.getElementById('aSong').load();
+  playSong();
+}
+
+function prevSong(){
+  document.getElementById('aSong').pause();
+  document.getElementById('aSong').removeAttribute("src");
+  document.getElementById('aSong').load();
+
+  index--;
+
+  if(index < 0 ){ index = playlist.length - 1; }
+
+  document.getElementById('aSong').src = "music/" + playlist[index];
+  document.getElementById('aSong').load();
+  playSong();
+}
